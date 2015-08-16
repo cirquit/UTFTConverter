7378ace1106e01d79e801912fbc58b5c381ca075
cirquit <alex.isenko@googlemail.com>
2015-08-16 14:37:52 +0200

    removed old cmd parsing because unix shell already handles wildcards superbly, tested the new one somewhat

--- src/Converter/JuicyConverter.hs
+++ src/Converter/JuicyConverter.hs
@@ -4,51 +4,52 @@ import Converter (jpgToC, jpgToRaw)
 
 import System.FilePath.Posix (takeBaseName, takeExtension, takeDirectory, (</>))
 import System.Directory      (doesDirectoryExist, getCurrentDirectory,
-                              getDirectoryContents)
+                              getDirectoryContents, createDirectoryIfMissing)
 import System.Environment    (getArgs, getProgName)
+
+type Args = [String]
 
 main :: IO()
 main = do
   args <- getArgs
+  print args
   case args of
-    (fpath:"/c":spath) -> do
-      savepath <- getSavePath spath
-      files    <- getFiles    fpath
-      mapM_ (jpgToC savepath) files
-    (fpath:"/r":spath) -> do
-      savepath <- getSavePath spath
-      files    <- getFiles    fpath
-      mapM_ (jpgToRaw savepath) files
+    (_)
+      | "/c" `elem` args, "/r" `notElem` args -> do
+        files    <- args `getFilesFor` "/c"
+        savepath <- getSavePath args
+        putStrLn $ "Got this savepath in /c: " ++ show savepath
+        mapM_ (jpgToC savepath) files
+      | "/r" `elem` args, "/c" `notElem` args -> do
+        files    <- args `getFilesFor` "/r"
+        savepath <- getSavePath args
+        putStrLn $ "Got this savepath in /r: " ++ show savepath
+        mapM_ (jpgToRaw savepath) files
     (_) -> getProgName >>= help
 
-getFiles :: FilePath -> IO [FilePath]
-getFiles fp = do
-  let name = takeBaseName  fp
-      dir  = takeDirectory fp
-      ext  = takeExtension fp
-  exists <- doesDirectoryExist dir
-  case (exists, name, ext) of
-    (False,   _,      _) -> putStrLn "This directory doesn't exist" >> return []
-    (_    , "*", ".jpg") -> do
-      contents <- getDirectoryContents dir
-      let files = filter (`notElem` [".", ".."]) contents
-          jpgs  = filter (\x -> takeExtension x == ".jpg") files
-      return $ map (dir </>) jpgs
-    (_    ,   _, ".jpg") -> return [fp]
+getFilesFor :: Args -> String -> IO [FilePath]
+getFilesFor l delim = go (takeWhile (delim /=) l)
+  where go :: [String] -> IO [FilePath]
+        go []     = return []
+        go (fp:xs) = do
+          case takeExtension fp of
+            (".jpg") -> go xs >>= \x -> return (fp : x)
+            (_)      -> putStrLn "This format is not supported!" >> go xs
 
-getSavePath :: [String] -> IO FilePath
-getSavePath ("/o":dir:[]) = do
-  exists <- doesDirectoryExist dir
-  if exists then return dir
-            else do
-              putStrLn "This directory doesn't exists, using the current instead"
-              getCurrentDirectory
-getSavePath _                 = getCurrentDirectory
+getSavePath :: Args -> IO FilePath
+getSavePath l = do
+  let savepath = dropWhile ("/o" /=) l
+  if length savepath == 2
+    then  do
+      let path = savepath !! 1
+      createDirectoryIfMissing True path
+      return path
+    else getCurrentDirectory
 
 help :: String -> IO ()
 help name = do
   putStrLn   "Usage:                                           "
-  putStrLn $ "      " ++ name ++ " <filespec> /c|r [/o <path]\n"
+  putStrLn $ "      " ++ name ++ " <filespec> /c|r [/o <path>]\n"
   putStrLn   "<filespec>:  File(s) to convert"
   putStrLn   "parameters: /c        - Create output as .c array files"
   putStrLn   "            /r        - Create output as .raw files"
